5857e29f3fe8ea1de3caa2af8e5af5209cd427bb
Anmoldeep <sainiannmol16@gmail.com>
2023-08-26 22:21:29 +0530

    CHange

--- index.js
+++ index.js
@@ -20,6 +20,15 @@ const corsOptions = {
 };
 
 //middlewares
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type, Authorization"
+  );
+  next();
+});
 app.use(json());
 app.use(cors(corsOptions));
 app.use(cookieParser());
