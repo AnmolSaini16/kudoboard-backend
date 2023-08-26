e5a9d86b286fc0df65867e796b152e316d983322
Anmoldeep <sainiannmol16@gmail.com>
2023-08-26 20:46:25 +0530

    Test

--- index.js
+++ index.js
@@ -10,24 +10,33 @@ import boardRoutes from "./routes/boardRoutes.js";
 dotenv.config();
 const app = express();
 
-const corsOptions = {
-  origin: true, //included origin as true
-  credentials: true, //included credentials as true
-  allowedOrigins: ["http://localhost:3000", "https://kudoboard.vercel.app/"],
-};
+// const corsOptions = {
+//   origin: true, //included origin as true
+//   credentials: true, //included credentials as true
+//   allowedOrigins: ["http://localhost:3000", "https://kudoboard.vercel.app/"],
+// };
 
+app.use(
+  cors({
+    origin: "https://kudoboard.vercel.app/",
+    credentials: true,
+    methods: "GET, POST, PUT, DELETE",
+  })
+);
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", req.headers.origin);
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Origin", "https://kudoboard.vercel.app/");
   res.header(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization, X-HTTP-Method-Override, Set-Cookie, Cookie"
   );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   next();
 });
 
 //middlewares
 app.use(json());
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 app.use(cookieParser());
 app.use(express.json());
 
