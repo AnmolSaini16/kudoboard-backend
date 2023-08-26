f481c65adf368d67bc012d36b6565e5d849dc990
Anmoldeep <sainiannmol16@gmail.com>
2023-08-26 23:03:54 +0530

    Final change as wished by manvi

--- index.js
+++ index.js
@@ -17,12 +17,11 @@ const corsOptions = {
     "https://kudoboard-api.onrender.com",
   ],
   credentials: true,
-  exposedHeaders: ["set-cookie"],
 };
 
 //middlewares
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", req.headers.origin);
   res.header("Access-Control-Allow-Credentials", true);
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
   res.header(
